992f82cf1e6235d4f78daeb15d5e00f97cb66b0a
sumit karki <sumitkarki@sumits-MacBook-Pro.local>
2025-12-30 20:59:14 -0600

    Project setup and Kafka integration up to Task 2

--- src/main/java/com/jpmc/midascore/MidasCoreApplication.java
+++ src/main/java/com/jpmc/midascore/MidasCoreApplication.java
@@ -2,8 +2,10 @@ package com.jpmc.midascore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.jpmc.midascore"})
 public class MidasCoreApplication {
 
     public static void main(String[] args) {
